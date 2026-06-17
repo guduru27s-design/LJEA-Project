98cc932c90ee98065db79edbc26b8749ce3a3151
guduru27s-design <guduru27s@ncssm.edu>
2026-06-17 13:14:26 -0400

    Refactor regression_formula parameter names for clarity

--- RegressionFormula.py
+++ RegressionFormula.py
@@ -1,18 +1,19 @@
-def regression_formula(precipitationDaily, precipitationWeekly, DA, CN, ADJ): 
+def regression_formula(ppt_daily, ppt_per_sum, DRNAREA, CN, ADJ): 
   S= (1000/CN) - 10
   B0= 1
   B1= 1
   B2= 1
   B3= 1
-  formula1= (precipitationDaily- 0.2 * S) ** 2
-  formula2= precipitationDaily -0.2 * S + S
-  formula3= DA * 27878400
+  formula1= (ppt_daily- 0.2 * S) ** 2
+  formula2= ppt_daily -0.2 * S + S
+  formula3= DRNAREA * 27878400
   runoff= (((formula1/formula2)/12) * formula3)/86400
-  antecedentPrecip= ((precipitationWeekly/12)*(DA * 27878400))/86400
-  interflow= ((ADJ + precipitationDaily) * 2323200 * DA)/86400
+  antecedentPrecip= ((ppt_per_sum/12)*(DRNAREA * 27878400))/86400
+  interflow= ((ADJ + ppt_daily) * 2323200 * DRNAREA)/86400
 
   finalFormula= B1 * runoff + B2 * antecedentPrecip + B3 * interflow + B0
   return finalFormula
 
-value= regression_formula(9,8,7,6,5)
-print(value)
+streamflow= regression_formula(9,8,7,6,5)
+print(streamflow)
+
