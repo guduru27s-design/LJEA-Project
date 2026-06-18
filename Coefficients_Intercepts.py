2202db0cb38420626ebd416ece3ee0808a5a065f
guduru27s-design <guduru27s@ncssm.edu>
2026-06-18 14:02:45 -0400

    Refactor regression calculation and output
    Refactor new_csv function to return individual regression coefficients and intercept. Update print statements to reflect new variable assignments.

--- Coefficients_Intercepts.py
+++ Coefficients_Intercepts.py
@@ -1,6 +1,14 @@
+
+#Import section 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-
+"""
+This function create a new csv file filling the spreadsheet with calculated values and the headers are runoff, antecedant precipitation, interflow, and streamflow
+Parameters:
+- csv file
+Returns:
+- new csv file with calculate values
+"""
 def new_csv(csv_file):
     df = pd.read_csv(csv_file)
 
@@ -12,8 +20,8 @@ def new_csv(csv_file):
 
 
     new_df["Runoff"] = (
-        (((df["Daily Precipitation"] - 0.2 * S) ** 2) /
-        (df["Daily Precipitation"] - 0.2 * S + S))
+        (((df["Daily Precipitation"] - df["Initial Abstraction"] * S) ** 2) /
+        (df["Daily Precipitation"] - (1-df["Initial Abstraction"]) * S ))
         / 12 * (df["DA"] * 27878400) / 86400
         )
 
@@ -27,6 +35,7 @@ def new_csv(csv_file):
     new_df["Interflow"] = (((ADJ + df["Daily Precipitation"]) * 2323200 * df["DA"]) / 86400)
 
     new_df["Streamflow"] = df["Streamflow"]
+    
 
     return new_df
 
@@ -38,13 +47,19 @@ def make_regression(new_df):
 
     lr = LinearRegression()
     lr.fit(X, Y)
+    
+    B1= lr.coef_[0]
+    B2= lr.coef_[1]
+    B3= lr.coef_[2]
+    B0= lr.intercept_
 
-    return lr.coef_, lr.intercept_
+    return B1, B2, B3, B0
 
-new_csv= new_csv(r"C:\Users\GUDUR\Downloads\fake spreadsheet - Sheet1 (1).csv")
-regression_results= make_regression(new_csv)
 
-print("B1 (Runoff):", regression_results[0][0])
-print("B2 (Antecedant Precip):", regression_results[0][1])
-print("B3 (Interflow):", regression_results[0][2])
-print("B0 (Intercept):", regression_results[1])
+new_csv= new_csv(r"C:\Users\GUDUR\Downloads\finalized fake spreadsheet - Sheet1 (2).csv")
+B1, B2, B3, B0= make_regression(new_csv)
+
+print("B1 (Runoff):", B1)
+print("B2 (Antecedant Precip):", B2)
+print("B3 (Interflow):", B3)
+print("B0 (Intercept):", B0)
