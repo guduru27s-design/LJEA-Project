9203cfb99dac5d0744a7b00c2d616f9e03a4fbe2
smgallop08 <smgallop08@gmail.com>
2026-06-19 10:56:30 -0400

    Refactor CN and IA calculations into functions

--- combo.py
+++ combo.py
@@ -437,87 +437,100 @@ prism_period_df = pd.DataFrame({
 
 print("\nPRISM Climate Data (PERIOD DATA):")
 print(prism_period_df)
-'''
----CN CALCULATION---
-'''
-Norm_CN = float(
-    ((int(df_chars.loc["LC11CRPHAY", "value"]) / 100) * 
-     ((df_chars.loc["SSURGOA", "value"] * 68 + df_chars.loc["SSURGOB", "value"]
-       * 76 + df_chars.loc["SSURGOC", "value"] * 
-       83 + df_chars.loc["SSURGOD", "value"] * 87) / 100))
-    + ((int(df_chars.loc["LC11FOREST", "value"]) / 100) *
-       ((df_chars.loc["SSURGOA", "value"] * 30 + df_chars.loc["SSURGOB", "value"]
-         * 60 + df_chars.loc["SSURGOC", "value"] *
-         75 + df_chars.loc["SSURGOD", "value"] * 81) / 100))
-    + ((int(df_chars.loc["LC11DEV", "value"]) / 100) * 
-       ((df_chars.loc["SSURGOA", "value"] * 77 + 
-         df_chars.loc["SSURGOB", "value"] * 85 + df_chars.loc["SSURGOC", "value"]
-         * 90 + df_chars.loc["SSURGOD", "value"] * 92) / 100))
-    + ((int(df_chars.loc["LC11GRASS", "value"]) / 100) * 
-       ((df_chars.loc["SSURGOA", "value"] * 45 + df_chars.loc["SSURGOB", "value"]
-         * 59 + df_chars.loc["SSURGOC", "value"] * 75 +
-         df_chars.loc["SSURGOD", "value"] * 85) / 100))
-    + ((int(df_chars.loc["LC11SHRUB", "value"]) / 100) * 
-       ((df_chars.loc["SSURGOA", "value"] * 35 + df_chars.loc["SSURGOB", "value"]
-         * 56 + df_chars.loc["SSURGOC", "value"] * 70 + 
-         df_chars.loc["SSURGOD", "value"] * 77) / 100))
-    + ((int(df_chars.loc["LC11IMP", "value"]) / 100) *
-       ((df_chars.loc["SSURGOA", "value"] * 98 + df_chars.loc["SSURGOB", "value"]
-         * 98 + df_chars.loc["SSURGOC", "value"] * 98 + 
-         df_chars.loc["SSURGOD", "value"] * 98) / 100))
-    - ((int(df_chars.loc["LC11BARE", "value"]) + 
-        int(df_chars.loc["LC11WATER", "value"]) + 
-        int(df_chars.loc["LC11WETLND", "value"])) / 100) * 
-    ((df_chars.loc["SSURGOA", "value"] * 59 + df_chars.loc["SSURGOB", "value"] 
-      * 72 + df_chars.loc["SSURGOC", "value"] * 82 +
-      df_chars.loc["SSURGOD", "value"] * 87) / 100) + SLOPECORRECTIONCN
-)
 
-Wet_CN = float(Norm_CN + 4)
-Dry_CN = float(Norm_CN - 4)
+def calculate_cn(df_chars, SLOPECORRECTIONCN):
+    '''
+    ---CN CALCULATION---
+    '''
 
-if ppt_per_sum >= 2:
-    Curve = Wet_CN
-    print(f"Calculated wet curve number (CN) for the watershed: {Curve}")
-elif ppt_per_sum < 2 and ppt_per_sum > 1:
-    Curve = Norm_CN
-    print(f"Calculated normal curve number (CN) for the watershed: {Curve}")
-elif ppt_per_sum <= 1:
-    Curve = Dry_CN
-    print(f"Calculated dry curve number (CN) for the watershed: {Curve}")
-else:
-    pass
+    Norm_CN = float(
+        ((int(df_chars.loc["LC11CRPHAY", "value"]) / 100) * 
+        ((df_chars.loc["SSURGOA", "value"] * 68 + df_chars.loc["SSURGOB", "value"]
+        * 76 + df_chars.loc["SSURGOC", "value"] * 
+        83 + df_chars.loc["SSURGOD", "value"] * 87) / 100))
+        + ((int(df_chars.loc["LC11FOREST", "value"]) / 100) *
+        ((df_chars.loc["SSURGOA", "value"] * 30 + df_chars.loc["SSURGOB", "value"]
+            * 60 + df_chars.loc["SSURGOC", "value"] *
+            75 + df_chars.loc["SSURGOD", "value"] * 81) / 100))
+        + ((int(df_chars.loc["LC11DEV", "value"]) / 100) * 
+        ((df_chars.loc["SSURGOA", "value"] * 77 + 
+            df_chars.loc["SSURGOB", "value"] * 85 + df_chars.loc["SSURGOC", "value"]
+            * 90 + df_chars.loc["SSURGOD", "value"] * 92) / 100))
+        + ((int(df_chars.loc["LC11GRASS", "value"]) / 100) * 
+        ((df_chars.loc["SSURGOA", "value"] * 45 + df_chars.loc["SSURGOB", "value"]
+            * 59 + df_chars.loc["SSURGOC", "value"] * 75 +
+            df_chars.loc["SSURGOD", "value"] * 85) / 100))
+        + ((int(df_chars.loc["LC11SHRUB", "value"]) / 100) * 
+        ((df_chars.loc["SSURGOA", "value"] * 35 + df_chars.loc["SSURGOB", "value"]
+            * 56 + df_chars.loc["SSURGOC", "value"] * 70 + 
+            df_chars.loc["SSURGOD", "value"] * 77) / 100))
+        + ((int(df_chars.loc["LC11IMP", "value"]) / 100) *
+        ((df_chars.loc["SSURGOA", "value"] * 98 + df_chars.loc["SSURGOB", "value"]
+            * 98 + df_chars.loc["SSURGOC", "value"] * 98 + 
+            df_chars.loc["SSURGOD", "value"] * 98) / 100))
+        - ((int(df_chars.loc["LC11BARE", "value"]) + 
+            int(df_chars.loc["LC11WATER", "value"]) + 
+            int(df_chars.loc["LC11WETLND", "value"])) / 100) * 
+        ((df_chars.loc["SSURGOA", "value"] * 59 + df_chars.loc["SSURGOB", "value"] 
+        * 72 + df_chars.loc["SSURGOC", "value"] * 82 +
+        df_chars.loc["SSURGOD", "value"] * 87) / 100) + SLOPECORRECTIONCN
+    )
+
+    Wet_CN = float(Norm_CN + 4)
+    Dry_CN = float(Norm_CN - 4)
+
+    if ppt_per_sum >= 2:
+        Curve = Wet_CN
+        print(f"Calculated wet curve number (CN) for the watershed: {Curve}")
+        return Curve
+    elif ppt_per_sum < 2 and ppt_per_sum > 1:
+        Curve = Norm_CN
+        print(f"Calculated normal curve number (CN) for the watershed: {Curve}")
+        return Curve
+    elif ppt_per_sum <= 1:
+        Curve = Dry_CN
+        print(f"Calculated dry curve number (CN) for the watershed: {Curve}")
+        return Curve
+    else:
+        pass
+
+calculate_cn(df_chars, SLOPECORRECTIONCN)
+
+def calculate_ia(df_chars, SLOPECORRECTIONIA):
+    '''
+    ---IA CALCULATION---
+    '''
 
 
-'''
----IA CALCULATION---
-'''
+    if "BSLDEM30FT" in df_chars.index:
+        BSLDEM30FT = float(df_chars.loc["BSLDEM30FT", "value"])
+        if BSLDEM30FT > 10 and BSLDEM30FT <= 30:
+            Ia = 0.2
+        elif BSLDEM30FT > 30 and BSLDEM30FT <= 45:
+            Ia = 0.15
 
+    if SLOPECORRECTIONIA != 0.0:
+        Norm_Ia = Ia + SLOPECORRECTIONIA
+        
+    Wet_Ia = float(Norm_Ia + 0.04)
+    Dry_Ia = float(Norm_Ia - 0.04)
+        
+    if ppt_per_sum >= 2:
+        IntialAbstraction = Wet_Ia
+        print(f"Calculated wet initial abstraction (Ia) for the watershed: {IntialAbstraction}")
+        return IntialAbstraction
+    elif ppt_per_sum < 2 and ppt_per_sum > 1:
+        IntialAbstraction = Norm_Ia
+        print(f"Calculated normal initial abstraction (Ia) for the watershed: {IntialAbstraction}")
+        return IntialAbstraction
+    elif ppt_per_sum <= 1:
+        IntialAbstraction = Dry_Ia
+        print(f"Calculated dry initial abstraction (Ia) for the watershed: {IntialAbstraction}")
+        return IntialAbstraction
+    else:
+        pass
 
-if "BSLDEM30FT" in df_chars.index:
-    BSLDEM30FT = float(df_chars.loc["BSLDEM30FT", "value"])
-    if BSLDEM30FT > 10 and BSLDEM30FT <= 30:
-        Ia = 0.2
-    elif BSLDEM30FT > 30 and BSLDEM30FT <= 45:
-        Ia = 0.15
-
-if SLOPECORRECTIONIA != 0.0:
-    Norm_Ia = Ia + SLOPECORRECTIONIA
-    
-Wet_Ia = float(Norm_Ia + 0.04)
-Dry_Ia = float(Norm_Ia - 0.04)
-    
-if ppt_per_sum >= 2:
-    IntialAbstraction = Wet_Ia
-    print(f"Calculated wet initial abstraction (Ia) for the watershed: {IntialAbstraction}")
-elif ppt_per_sum < 2 and ppt_per_sum > 1:
-    IntialAbstraction = Norm_Ia
-    print(f"Calculated normal initial abstraction (Ia) for the watershed: {IntialAbstraction}")
-elif ppt_per_sum <= 1:
-    IntialAbstraction = Dry_Ia
-    print(f"Calculated dry initial abstraction (Ia) for the watershed: {IntialAbstraction}")
-else:
-    pass
+calculate_ia(df_chars, SLOPECORRECTIONIA)
 
 '''Adjustment factor calculation for the CN based on the month of the year and yesterday's precipitation'''    
 def retrieve_month(ENDDATE):
