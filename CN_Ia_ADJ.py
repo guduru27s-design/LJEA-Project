0014d60e8ed231a15dcd4f735f6e4c3f376eb9f1
smgallop08 <smgallop08@gmail.com>
2026-06-22 09:59:45 -0400

    Adjust Ia value based on BSLDEM30FT range

--- CN_Ia_ADJ.py
+++ CN_Ia_ADJ.py
@@ -68,7 +68,7 @@ def calculate_ia_for_calibration(df_chars):
         if BSLDEM30FT > 10 and BSLDEM30FT <= 30:
             Ia = 0.2
         elif BSLDEM30FT > 30 and BSLDEM30FT <= 45:
-            Ia = 0.15
+            Ia = 0.22
 
     if SLOPECORRECTIONIA != 0.0:
         Norm_Ia = Ia + -.02
