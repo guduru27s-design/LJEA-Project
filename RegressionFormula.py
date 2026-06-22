ff3932d5ec2582720b572b02995b3eb016755b6a
guduru27s-design <guduru27s@ncssm.edu>
2026-06-22 15:08:51 -0400

    Update RegressionFormula.py

--- RegressionFormula.py
+++ RegressionFormula.py
@@ -1,3 +1,4 @@
+
 def regression_formula(ppt_daily, ppt_per_sum, DRNAREA, Curve, Adjustments, InitialAbstraction, B0, B1, B2, B3): 
   S = (1000 / Curve) - 10
   B0 = B0
@@ -9,11 +10,10 @@ def regression_formula(ppt_daily, ppt_per_sum, DRNAREA, Curve, Adjustments, Init
   formula3 = DRNAREA * 27878400
   runoff = (((formula1 / formula2) / 12) * formula3) / 86400
   baseflow = ((ppt_per_sum / 12) * (DRNAREA * 27878400)) / 86400
-  interflow = ((adjustments + ppt_daily) * 2323200 * DRNAREA) / 86400
+  interflow = ((Adjustments + ppt_daily) * 2323200 * DRNAREA) / 86400
 
   finalFormula= B1 * runoff + B2 * baseflow + B3 * interflow + B0
   return finalFormula
 
-streamflow = regression_formula(9,8,7,6,5,4,3,2,1)
-print(f"The streamflow for {LATITUDE}, {LONGITUDE} is {streamflow} cfs.")
-
+streamflow = regression_formula(0.04589667722,0.04589667722,67,55.17338,-0.02,0.16, B0, B1, B2, B3)
+print(streamflow)
