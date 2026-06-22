28fd596ee75d183058d86b09da63be9560e20622
smgallop08 <smgallop08@gmail.com>
2026-06-22 13:36:18 -0400

    Refactor regression_formula parameters and calculations
    Updated parameter names and adjusted formula calculations for runoff and baseflow.

--- RegressionFormula.py
+++ RegressionFormula.py
@@ -1,19 +1,19 @@
-def regression_formula(ppt_daily, ppt_per_sum, DRNAREA, CN, ADJ, B0, B1, B2, B3): 
-  S = (1000/CN) - 10
+def regression_formula(ppt_daily, ppt_per_sum, DRNAREA, Curve, Adjustments, InitialAbstraction, B0, B1, B2, B3): 
+  S = (1000 / Curve) - 10
   B0 = B0
   B1 = B1
   B2 = B2
   B3 = B3
-  formula1 = (ppt_daily- 0.2 * S) ** 2
-  formula2 = ppt_daily -0.2 * S + S
+  formula1 = (ppt_daily - InitialAbstraction * S) ** 2
+  formula2 = ppt_daily - InitialAbstraction * S
   formula3 = DRNAREA * 27878400
-  runoff = (((formula1/formula2)/12) * formula3)/86400
-  antecedentPrecip = ((ppt_per_sum/12) * (DRNAREA * 27878400))/86400
-  interflow = ((ADJ + ppt_daily) * 2323200 * DRNAREA)/86400
+  runoff = (((formula1 / formula2) / 12) * formula3) / 86400
+  baseflow = ((ppt_per_sum / 12) * (DRNAREA * 27878400)) / 86400
+  interflow = ((ADJ + ppt_daily) * 2323200 * DRNAREA) / 86400
 
-  finalFormula= B1 * runoff + B2 * antecedentPrecip + B3 * interflow + B0
+  finalFormula= B1 * runoff + B2 * baseflow + B3 * interflow + B0
   return finalFormula
 
-streamflow= regression_formula(9,8,7,6,5,4,3,2,1)
-print(streamflow)
+streamflow = regression_formula(9,8,7,6,5,4,3,2,1)
+print(f"The streamflow for {LATITUDE}, {LONGITUDE} is {streamflow} cfs.")
 
