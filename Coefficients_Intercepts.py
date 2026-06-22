9f2076396a0ab16e6538a248ba7ad0ef62628896
guduru27s-design <guduru27s@ncssm.edu>
2026-06-22 13:44:00 -0400

    Update Coefficients_Intercepts.py

--- Coefficients_Intercepts.py
+++ Coefficients_Intercepts.py
@@ -1,7 +1,10 @@
 
 #Import section 
 import pandas as pd
+from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
 """
 This function create a new csv file filling the spreadsheet with calculated values and the headers are runoff, antecedant precipitation, interflow, and streamflow
 Parameters:
@@ -11,26 +14,30 @@ Returns:
 """
 def new_csv(csv_file):
     df = pd.read_csv(csv_file)
+ 
+    CN = 50
+    S = (1000 / CN) - 10
+    ADJ= 0.25
 
     new_df = pd.DataFrame()
 
 
     new_df["Runoff"] = (
-        (((df["daily_precip"] - df["IA value"] * ((100/ df["CN value"]) -10)) ** 2) /
-        (df["daily_precip"] - (1-df["IA value"]) * ((100/ df["CN value"]) -10) ))
-        / 12 * (df["drainage_area"] * 27878400) / 86400
+        (((df["Daily Precipitation"] - df["Initial Abstraction"] * S) ** 2) /
+        (df["Daily Precipitation"] - (1-df["Initial Abstraction"]) * S ))
+        / 12 * (df["DA"] * 27878400) / 86400
         )
 
 
     new_df["Antecedant Precip"] = (
-            (df["weekly_precip"] / 12)
-            * (df["drainage_area"] * 27878400)
+            (df["Weekly Precipitation"] / 12)
+            * (df["DA"] * 27878400)
             / 86400
         )
         
-    new_df["Interflow"] = (((df["ADJ value"] + df["daily_precip"]) * 2323200 * df["drainage_area"]) / 86400)
+    new_df["Interflow"] = (((ADJ + df["Daily Precipitation"]) * 2323200 * df["DA"]) / 86400)
 
-    new_df["Streamflow"] = df["streamflow value"]
+    new_df["Streamflow"] = df["Streamflow"]
     
 
     return new_df
@@ -40,28 +47,27 @@ def make_regression(new_df):
 
     X = new_df[["Runoff", "Antecedant Precip", "Interflow"]]
     Y = new_df["Streamflow"]
+    
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
     lr = LinearRegression()
-    lr.fit(X, Y)
-    
+    lr.fit(X_train, Y_train)
+
     B1= lr.coef_[0]
     B2= lr.coef_[1]
     B3= lr.coef_[2]
     B0= lr.intercept_
+    
+    R2= r2_score(Y_test, lr.predict(X_test))
 
-    return B1, B2, B3, B0
+    return B1, B2, B3, B0, R2
 
 
 new_csv= new_csv(r"C:\Users\GUDUR\Downloads\finalized fake spreadsheet - Sheet1 (2).csv")
-B1, B2, B3, B0= make_regression(new_csv)
+B1, B2, B3, B0, R2= make_regression(new_csv)
 
 print("B1 (Runoff):", B1)
 print("B2 (Antecedant Precip):", B2)
 print("B3 (Interflow):", B3)
 print("B0 (Intercept):", B0)
-
-
-
-
-
-
+print("R squared:", R2)
