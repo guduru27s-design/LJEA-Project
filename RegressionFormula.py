e3aeb665d1babb9cd3770d316cb0029c7933400f
smgallop08 <smgallop08@gmail.com>
2026-06-23 09:59:28 -0400

    Add conditional check for runoff calculation

--- RegressionFormula.py
+++ RegressionFormula.py
@@ -5,10 +5,14 @@ def regression_formula(ppt_daily, ppt_per_sum, DRNAREA, Curve, Adjustments, Init
   B1 = B1
   B2 = B2
   B3 = B3
+      
   formula1 = (ppt_daily - InitialAbstraction * S) ** 2
   formula2 = ppt_daily + (1 - InitialAbstraction) * S
   formula3 = DRNAREA * 27878400
-  runoff = (((formula1 / formula2) / 12) * formula3) / 86400
+  if ppt_daily <= (InitialAbstraction * S):
+    runoff = 0
+  else:
+    runoff = (((formula1 / formula2) / 12) * formula3) / 86400
   baseflow = ((ppt_per_sum / 12) * (DRNAREA * 27878400)) / 86400
   interflow = ((Adjustments + ppt_daily) * 2323200 * DRNAREA) / 86400
 
